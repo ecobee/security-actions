b9ff09ee178f58936bcb4b130f932c69d9f41f5a
Ori Talmor <talmorori@gmail.com>
2022-02-25 17:28:35 -0500

    Adding Action Output

--- lib/ecoscan.sh
+++ lib/ecoscan.sh
@@ -33,7 +33,7 @@ then
     curl -sfL https://raw.githubusercontent.com/securego/gosec/master/install.sh | sh -s v2.9.5
 
     #Run gosec
-    _result=$(bin/gosec -fmt=text $INPUT_DIR)
+    echo "::set-output name=ecoscan_result::$(bin/gosec -fmt=text $INPUT_DIR)"
 
 elif [[ $INPUT_LANG =~ ^(js|javascript)$ ]]
 then
@@ -73,6 +73,6 @@ fi
 
 
 # Output scan results
-echo "echoing result"
-echo $_result
-echo "::set-output name=ecoscan_result::$_result"
+#echo "echoing result"
+#echo $_result
+#echo "::set-output name=ecoscan_result::$_result"
